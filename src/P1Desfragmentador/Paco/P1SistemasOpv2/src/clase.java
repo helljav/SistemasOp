d2cfd21281d08af8cfb0d7f27fb549828d3dc214
helljav <miromancequimico_004@hotmail.com>
2019-02-04 19:09:32 -0600

    Primera practica hecha

---
+++ src/P1Desfragmentador/Paco/P1SistemasOpv2/src/clase.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class clase {
+	public static void main(String[] args) {
+		ArrayList<Integer> General = new ArrayList<>(3);
+		System.out.println(General.get(6));
+		
+	}
+
+}
